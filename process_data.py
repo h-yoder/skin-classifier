aa982263821e65f41d83eedca3ae17d00fc42a0a
h-yoder <hyoder@mit.edu>
2022-04-25 17:11:12 -0400

    making data dictionary with file name to diagnosis

--- process_data.py
+++ process_data.py
@@ -1,9 +1,30 @@
-import numpy as np
+import pandas as pd
+import os
+import json
 
-cat = 1 + 1
+dir_ = os.getcwd()
 
-for i in range(cat):
-    print('hi')
+malig_path = dir_ + '/malignant'
+malig_desc_path = dir_ + '/m_desc'
+
+ben_path = dir_ + '/benign'
+ben_desc_path = dir_ + '/b_desc'
+
+data = {}
+
+all_files = []
+
+for f in os.listdir(malig_desc_path):
+    if os.path.isfile(os.path.join(malig_desc_path, f)):
+        all_files.append(f)
+
+for f in os.listdir(ben_desc_path):
+    if os.path.isfile(os.path.join(ben_desc_path, f)):
+        all_files.append(f)
+
+for f in all_files:
+    with open(f) as json_file:
+        file = json.load(json_file)
+        data[file['name']] = file['meta']['clinical']['benign_malignant']
 
 
-    
